d835a96caf6ec4184c1ab0548ca02f88066ef7f7
Rushika pawar <51110842+codex-pro@users.noreply.github.com>
2019-08-22 23:55:50 +0545

    Create items.py

---
+++ items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class DatabaseTestItem(scrapy.Item):
+    # define the fields for your item here like:
+    product_name = scrapy.Field()
+    pass
